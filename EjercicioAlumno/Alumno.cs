51cba99c0e52351734ea45e98c20f128ac543421
Software Especial <software@ulsa.ulsa-noroeste.edu.mx>
2019-08-19 12:27:51 -0700

    Agregar archivos de proyecto.

---
+++ EjercicioAlumno/Alumno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAlumno
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Cumple { get; set; }
+
+
+        public Alumno()
+
+        {
+            Nombre = "Pendiente";
+            Matricula = "Sin asignar";
+            Direccion = "pendiente";
+            Telefono = "Sin asignar";
+            Cumple = "Sin asignar";
+          
+            
+
+        }
+
+
+    }
+}
---
+++ EjercicioAlumno/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAlumno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno = new Alumno();
+
+            alumno.Nombre = "Ernesto";
+            alumno.Matricula = "189216";
+            alumno.Direccion = "Nazas 897";
+            alumno.Telefono = "1774178";
+            alumno.Cumple = "17 de agosto de 1999";
+
+
+
+
+            Console.WriteLine("Alumno: ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Nombre: " + alumno.Nombre);
+            Console.WriteLine("Matricula: " + alumno.Matricula);
+            Console.WriteLine("Direccion: " + alumno.Direccion);
+            Console.WriteLine("Telefono: " + alumno.Telefono);
+            Console.WriteLine("Fecha de nacimiento: " + alumno.Cumple);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
